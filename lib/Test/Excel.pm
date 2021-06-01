9cb244b01813b17ca1b7582685f8a185fe87c8bc
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2021-06-01 14:36:06 +0100

    - Updated MANIFEST to include the new test data.

--- lib/Test/Excel.pm
+++ lib/Test/Excel.pm
@@ -1,6 +1,6 @@
 package Test::Excel;
 
-$Test::Excel::VERSION   = '1.47';
+$Test::Excel::VERSION   = '1.48';
 $Test::Excel::AUTHORITY = 'cpan:MANWAR';
 
 =head1 NAME
@@ -9,7 +9,7 @@ Test::Excel - Interface to test and compare Excel files (.xls/.xlsx).
 
 =head1 VERSION
 
-Version 1.47
+Version 1.48
 
 =cut
 
